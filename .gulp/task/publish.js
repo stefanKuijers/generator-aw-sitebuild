3d5492441843cff68280e1ffc11160ca43200f4e
skuijers <stefan@arteries.hu>
2015-09-29 12:50:15 +0200

    PATCH | publish new version patch

--- .gulp/task/publish.js
+++ .gulp/task/publish.js
@@ -10,13 +10,6 @@ module.exports = function (gulp, plugins, config) {
 			    console.log( 'BUILD_out:' + stdout );
 			    console.log( 'BUILD_err:' + stderr );
 
-			    // return gulp.src( './*' )
-			    // 	.pipe( plugins.plumber() )
-			    // 	.pipe( plugins.git.commit(
-			    // 		'PATCH | publish new version patch',
-			    // 		{ args: '-A' }
-			    // 	) );
-
         		return plugins.exec('git add -A', function (err, stdout, stderr) {
 				    console.log( 'ADD_out:' + stdout );
 			    	console.log( 'ADD_err:' + stderr );
@@ -24,26 +17,25 @@ module.exports = function (gulp, plugins, config) {
 					return plugins.exec('git commit -m "PATCH | publish new version patch"', function (err, stdout, stderr) {
 					    console.log( 'COMMIT_out:' + stdout );
 			    		console.log( 'COMMIT_err:' + stderr );
-			    		
-					    return true;
+
+						return plugins.exec('npm version patch', function (err, stdout, stderr) {
+						    console.log( 'PATCH_out: ' + stdout );
+						    console.log( 'PATCH_err: ' + stderr );
+
+			    			
+							return plugins.exec('npm publish', function (err, stdout, stderr) {
+							    console.log( 'PUBLISH_out: ' + stdout );
+							    console.log( 'PUBLISH_err: ' + stderr );
+
+				    			return true;
+							} );
+						} );
+					    
 					} ) ;
 				    
 				} ) ;
 
 
-				// return plugins.exec('npm version patch', function (err, stdout, stderr) {
-				//     console.log( 'PATCH: ' + stdout );
-				//     console.log( 'PATCH: ' + stderr );
-	   //  			console.log( 'PATCH: ' + err );
-
-	    			
-				// 	return plugins.exec('npm publish', function (err, stdout, stderr) {
-				// 	    console.log( 'PUBLISH: ' + stdout );
-				// 	    console.log( 'PUBLISH: ' + stderr );
-		  //   			console.log( 'PUBLISH: ' + err );
-		  //   			return true;
-				// 	});
-				// });
 			});
 
 
