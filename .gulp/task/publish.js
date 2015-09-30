3e58b6e793485e96c30e5bca1c16f2e1e72dbaab
skuijers <stefan@kuijers.nl>
2015-09-30 08:48:01 +0200

    PATCH | publish new version patch

--- .gulp/task/publish.js
+++ .gulp/task/publish.js
@@ -5,32 +5,29 @@
 */
 module.exports = function (gulp, plugins, config) {
     return function() {
+        function log( cmd, out, err ) {
+            console.log( cmd + out );
+            console.log( cmd + err );
+        }
 
     	return plugins.exec('gulp build', function (err, stdout, stderr) {
-		    console.log( 'BUILD_out:' + stdout );
-		    console.log( 'BUILD_err:' + stderr );
+		    log( 'BUILD: ', stdout, stderr );
 
-		    return plugins.exec('npm version patch', function (err, stdout, stderr) {
-			    console.log( 'PATCH_out: ' + stdout );
-			    console.log( 'PATCH_err: ' + stderr );
+            return plugins.exec('git add -A', function (err, stdout, stderr) {
+                log( 'ADD: ', stdout, stderr );
 
-	    		return plugins.exec('git add -A', function (err, stdout, stderr) {
-				    console.log( 'ADD_out:' + stdout );
-			    	console.log( 'ADD_err:' + stderr );
+                return plugins.exec('git commit -m "PATCH | publish new version patch"', function (err, stdout, stderr) {
+                    log( 'COMMIT: ', stdout, stderr );
 
-					return plugins.exec('git commit -m "PATCH | publish new version patch"', function (err, stdout, stderr) {
-					    console.log( 'COMMIT_out:' + stdout );
-			    		console.log( 'COMMIT_err:' + stderr );
+        		    return plugins.exec('npm version patch', function (err, stdout, stderr) {
+                        log( 'PATCH: ', stdout, stderr );
 
 			    		plugins.exec('git push origin master', function (err, stdout, stderr) {
-						    console.log( 'PUSH_out:' + stdout );
-				    		console.log( 'PUSH_err:' + stderr );
-				    	} );
+                            log( 'PUSH: ', stdout, stderr );				    	
+                        } );
 		    			
 						return plugins.exec('npm publish', function (err, stdout, stderr) {
-						    console.log( 'PUBLISH_out: ' + stdout );
-						    console.log( 'PUBLISH_err: ' + stderr );
-
+                            log( 'PUBLISH: ', stdout, stderr );
 			    			return true;
 						} );
 					} );
